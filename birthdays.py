c30bd4024dde0afa2934ac447ed3cabe431c34fb
Bob <kagerawks@gmail.com>
2026-06-06 13:06:07 -0400

    new file:   birthdays.py

---
+++ birthdays.py
@@ -0,0 +1,16 @@
+birthdays = {'Alice': 1990, 'Bob': 1985, 'Charlie': 1992}
+
+while True:
+    print('Enter a name (or "exit" to quit):')
+    name = input()
+    if name == '':
+        break
+
+    if name in birthdays:
+        print(str(birthdays[name]) + ' is the birth year of ' + name)
+    else:
+        print('Name not found.')
+        print('Enter the birth year of ' + name + ':')
+        birth_year = input()
+        birthdays[name] = int(birth_year)
+        print('Birth year of ' + name + ' has been added.')
